8b0b66fc84a2aec9030e9c9e96e4a7ade51e4532
YoussefIbrahim9 <youssefhasssan873@gmail.com>
2024-06-21 14:46:02 +0200

    footer error fixed

---
+++ Js/footer.js
@@ -0,0 +1,7 @@
+var subscribeButton = document.getElementById("footerSubscribeBttn");
+var subscribtionInput = document.getElementById("subscribtionEmail");
+
+
+subscribeButton.addEventListener("click",function(){
+    subscribtionInput.value = "";
+})
